541fe6946ad04989248708c388f60706aa0c450d
Akshat42 <akshatdivya03@gmail.com>
2022-03-01 21:18:44 +0530

    Add Intro component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import ContactUs from './components/contactUs/ContactUs';
 import Footer from './components/footer/footer';
 import Header from './components/header/header';
+import Intro from './components/intro/Inro';
 import LinearGradient from './UI/LinearGradient';
 
 function App() {
     return (
         <>
             <Header></Header>
+            <Intro></Intro>
             <LinearGradient></LinearGradient>
             <ContactUs></ContactUs>
             <LinearGradient></LinearGradient>
---
+++ src/components/intro/Inro.js
@@ -0,0 +1,34 @@
+import './Intro.css';
+
+function Intro() {
+    var companyName = 'Infrrd';
+
+    return (
+        <section id="intro">
+            <p class="name">
+                Hi, my name is <span>Akshat Divya.</span>
+            </p>
+
+            <h2>I build Web Apps.</h2>
+
+            <p>
+                I'm a front-end developer specializing in HTML, CSS, Angular and
+                React.
+            </p>
+
+            <p>
+                {`Currently, I'm working at `}
+                <a
+                    rel="noreferrer"
+                    href="https://www.infrrd.ai"
+                    target="_blank"
+                >
+                    {companyName}
+                </a>
+                , developing web application for a real estate client.
+            </p>
+        </section>
+    );
+}
+
+export default Intro;
